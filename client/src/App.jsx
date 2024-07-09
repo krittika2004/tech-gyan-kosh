ed95bbc90d22b432ad6905e7f8b3fd3ae1ff0344
Krittika <krittikajari@gmail.com>
2024-07-09 17:02:35 +0530

    Routes setup done for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
+import Dashboard from "./pages/Dashboard"
 
-
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path:"/about",
+      element:<About/>
+    },
+    {
+      path: "/sign-up",
+      element:<SignUp/>
+    },
+    {
+      path:"/sign-in",
+      element:<SignIn/>
+    },
+    {
+      path:"/dashboard",
+      element:<Dashboard/>
+    }
+  ]
+)
 
 function App() {
   
 
   return (
     <>
+    <RouterProvider router={router} />
+
      <div>
      <h1 className='text-3xl text-red-900 bg-gray-400 text-center'>TechGyanKosh</h1>
      </div>

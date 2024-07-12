d803dd9d4ff805c17cd2b82e33312b8dab80bbbd
Krittika <krittikajari@gmail.com>
2024-07-12 17:48:21 +0530

    Db Connected and Schema created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
+mongoose
+.connect(process.env.MONGO)
+.then(
+   ()=>{
+    console.log("MongoDB connected");
+   }
+)
+.catch((err)=>{
+    console.log(err);
+})
+
 app.listen(3000, ()=>{
     console.log(`Server listening at port ${port}`);
 });

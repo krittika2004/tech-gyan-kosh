d62ba18f5528ceaffc244bf46473ba86741eb164
Krittika <krittikajari@gmail.com>
2024-07-13 19:08:37 +0530

    db finally connected

--- api/index.js
+++ api/index.js
@@ -1,21 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-
+import authRoutes from './routes/auth.routes.js';
 dotenv.config();
-
 const app = express();
 const port = 3000;
+
+const uri= "mongodb+srv://krittikajari:krittika@techgyaan.hx8pfqy.mongodb.net/techGyaan?retryWrites=true&w=majority&appName=techGyaan";
+//console.log('MongoDB connection string:',uri);
+
 mongoose
-.connect(process.env.MONGO)
-.then(
-   ()=>{
-    console.log("MongoDB connected");
-   }
-)
-.catch((err)=>{
+  .connect(uri)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
     console.log(err);
+  });
+  
+app.use(express.urlencoded({ extended: true }));
+//test route
+app.use(express.json());
+
+app.get('/test',(req,res)=>{
+    res.json({message: 'Api is working'});
 })
+
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, ()=>{
     console.log(`Server listening at port ${port}`);

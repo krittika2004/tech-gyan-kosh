4bf047672c6ba08fcdbe319d251d0d1723b10e95
Krittika <krittikajari@gmail.com>
2024-07-14 18:16:29 +0530

    sign in functionality + cookie creation using jwt done+ error handler function creted

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
 
@@ -22,3 +25,38 @@ export const signup = async (req, res,next) => {
     next(err);
   }
 };
+
+export const signin = async (req,res,next)=>{
+  const { email, password } = req.body;
+  if(!email || !password || email == '' || password == '') {
+    return next(errorHandler(400,'All fields are required'));
+  }
+  try{
+    //try to find the user from the db with the given email
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,'User Not Found!! '))
+    }
+    ///if user with the given email found then compare the password using compare sync of bycryptjs 
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    //if not valid==>error
+
+    if(!validPassword){
+      return next(errorHandler(400,'Invalid Password')); //400 = bad request
+    }
+    //now email add password matched - save this encrypted email password to cookie of the browser-- cookie assigned based on secret key 
+    //secret key is unique to u
+    const sec = "techGyaan";
+    //if successful don't want to send password with valid user
+    const {password: pass, ...userWithoutPassword} = validUser._doc;
+    const token = jwt.sign(
+      {id: validUser._id}, sec);
+      res.status(200).cookie('access_token', token, {
+      httpOnly: true
+      }).json(userWithoutPassword);
+
+  }catch(err){
+    next(err);
+  }
+}
